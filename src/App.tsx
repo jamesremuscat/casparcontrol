d40bda64f2c5b4452b42db7d994c3b98df2a57e5
James Muscat <jamesremuscat@gmail.com>
2023-02-09 21:46:32 +0000

    Tidy up.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import nodeLogo from "./assets/node.svg"
-import { useState } from 'react'
-import './App.scss'
+import nodeLogo from "./assets/node.svg";
+import { useState } from 'react';
 
-console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron}!`)
+// eslint-disable-next-line no-console
+console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron}!`);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <div>
-        <a href="https://github.com/electron-vite/electron-vite-react" target="_blank">
-          <img src="./electron-vite.svg" className="logo" alt="Electron + Vite logo" />
+        <a
+          href="https://github.com/electron-vite/electron-vite-react"
+          rel="noreferrer"
+          target="_blank">
+          <img
+            alt="Electron + Vite logo"
+            className="logo"
+            src="./electron-vite.svg" />
         </a>
       </div>
       <h1>Electron + Vite + React</h1>
@@ -27,10 +33,13 @@ function App() {
         Click on the Electron + Vite logo to learn more
       </p>
       <div className="flex-center">
-        Place static files into the<code>/public</code> folder <img style={{ width: "5em" }} src={nodeLogo} alt="Node logo" />
+        Place static files into the<code>/public</code> folder <img
+          alt="Node logo"
+          src={nodeLogo}
+          style={{ width: "5em" }} />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
