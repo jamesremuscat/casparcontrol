85b3366e75432f84a30e877eff4f6fd0658991e2
James Muscat <jamesremuscat@gmail.com>
2023-02-27 11:06:05 +0000

    Break media list down into a hierarchy.

--- src/modules/serverBrowser/__tests__/functions.test.ts
+++ src/modules/serverBrowser/__tests__/functions.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from 'vitest';
-import { MediaItem } from '@/modules/caspar';
+import { MediaItem, MediaType } from '@/modules/caspar';
 import { createPathHierarchy, PathHierarchy } from '../functions';
 
 interface Expectation {
@@ -15,10 +15,101 @@ describe('Server browser functions', () => {
         media: [],
         paths: {
           name: '/',
-          children: [],
+          children: {},
           items: []
         }
-      }
+      },
+      {
+        media: [
+          {
+            clip: 'TOPDIR/NEXTDIR/FILE.MOV',
+            datetime: '1677494687',
+            framerate: '1/25',
+            frames: '250',
+            size: '123456',
+            type: MediaType.MOVIE
+          }
+        ],
+        paths: {
+          name: '/',
+          items: [],
+          children: {
+            'TOPDIR': {
+              name: 'TOPDIR',
+              items: [],
+              children: {
+                'NEXTDIR': {
+                  name: 'NEXTDIR',
+                  children: {},
+                  items: [
+                    {
+                      clip: 'TOPDIR/NEXTDIR/FILE.MOV',
+                      datetime: '1677494687',
+                      framerate: '1/25',
+                      frames: '250',
+                      size: '123456',
+                      type: MediaType.MOVIE
+                    }
+                  ]
+                }
+              }
+            }
+          }
+        }
+      },
+      {
+        media: [
+          {
+            clip: 'TOPLEVELFILE.MOV',
+            datetime: '1677494687',
+            framerate: '1/25',
+            frames: '250',
+            size: '123456',
+            type: MediaType.MOVIE
+          },
+          {
+            clip: 'TOPDIR/NEXTDIR/FILE.MOV',
+            datetime: '1677494687',
+            framerate: '1/25',
+            frames: '250',
+            size: '123456',
+            type: MediaType.MOVIE
+          }
+        ],
+        paths: {
+          name: '/',
+          items: [{
+            clip: 'TOPLEVELFILE.MOV',
+            datetime: '1677494687',
+            framerate: '1/25',
+            frames: '250',
+            size: '123456',
+            type: MediaType.MOVIE
+          }],
+          children: {
+            'TOPDIR': {
+              name: 'TOPDIR',
+              items: [],
+              children: {
+                'NEXTDIR': {
+                  name: 'NEXTDIR',
+                  children: {},
+                  items: [
+                    {
+                      clip: 'TOPDIR/NEXTDIR/FILE.MOV',
+                      datetime: '1677494687',
+                      framerate: '1/25',
+                      frames: '250',
+                      size: '123456',
+                      type: MediaType.MOVIE
+                    }
+                  ]
+                }
+              }
+            }
+          }
+        }
+      },
     ];
 
     expectations.forEach(
--- src/modules/serverBrowser/functions.ts
+++ src/modules/serverBrowser/functions.ts
@@ -1,17 +1,49 @@
+import path from 'path';
 import { MediaItem } from '../caspar';
 
 export interface PathHierarchy {
   name: string,
-  children: PathHierarchy[],
+  children: Record<string, PathHierarchy>,
   items: MediaItem[]
 }
+
+const splitPath = (p: string) => p.split(path.sep);
+
+const getPath = (parent: PathHierarchy, pathParts: string[]): PathHierarchy => {
+
+  if (pathParts.length === 0) {
+    return parent;
+  }
+
+  const nextPath = pathParts[0];
+
+  if (!parent.children[nextPath]) {
+    parent.children[nextPath] = {
+      name: nextPath,
+      children: {},
+      items: []
+    };
+  }
+
+  return getPath(parent.children[nextPath], pathParts.slice(1));
+};
 
 export const createPathHierarchy = (media: MediaItem[]) : PathHierarchy => {
   const root = {
     'name': '/',
-    children: [],
+    children: {},
     items: []
   };
+
+  media.forEach(
+    m => {
+      const pathParts = splitPath(m.clip);
+
+      const parentDir = getPath(root, pathParts.slice(0, -1));
+
+      parentDir.items.push(m);
+    }
+  );
 
   return root;
 };
